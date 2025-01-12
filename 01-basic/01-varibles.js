5f08af7c23da202d28ca2e176fc42119bfc3f181
TEJASPAWAR2005 <tejaspawar9325@gmail.com>
2025-01-12 15:19:51 +0000

    container config done

---
+++ 01-basic/01-varibles.js
@@ -0,0 +1,8 @@
+ const accountId=1234
+let accountemail="tejas@9233.com"
+var accountpassword="98866"
+accountcity="nashik"
+ let accountstate;
+
+
+console.table([accountId, accountemail,accountpassword,accountcity,accountstate]);
---
+++ 01-basic/02-tejas.js
@@ -0,0 +1,8 @@
+let score =null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnumber = number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
